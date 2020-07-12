bd74d21697d096665c500aed9603c807cde54633
Ezwann <celestin.eudeline@gmail.com>
2020-07-12 23:15:14 +0200

    V1.0 - Under an hour time (can be improved)

---
+++ script.js
@@ -0,0 +1,85 @@
+var field = document.querySelector('table tbody');
+var cols = rows = 40;
+var grid = buildField(cols, rows);
+var running = false;
+var currentGen = 0;
+var saveGrids = [];
+
+function buildField(cols, rows) {
+    var arr = new Array(cols);
+    for(let i = 0; i < arr.length; i++) {
+       arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+function populateField() {
+    for(var i = 0; i < grid.length; i++) {
+        for(var j = 0; j < grid[i].length; j++) {
+            if(Math.random() > 0.83)
+                grid[i][j] = true;
+            else
+                grid[i][j] = false;
+        }
+    }
+    saveGrid();
+}
+
+function render() {
+    var tableRendered = "";
+    for(var i = 0; i < grid.length; i++) {
+        tableRendered += "<tr>"
+        for(var j = 0; j < grid[i].length; j++) {
+            tableRendered += `<td style="background-color: ${(grid[i][j])?"black":"white"}"></td>`;
+        }
+        tableRendered += "</tr>"
+    }
+    field.innerHTML = tableRendered;
+}
+
+function makeItLive() {
+    var nextGrid = grid;
+    for(var i = 0; i < grid.length; i++) {
+        for(var j = 0; j < grid[i].length; j++) {
+            let totalNeighbours = countNeighbourhood(i, j, grid[i][j]);
+            if(totalNeighbours <= 1 || totalNeighbours >= 4) nextGrid[i][j] = false;
+            else if(totalNeighbours == 3) nextGrid[i][j] = true;
+        }
+    }
+    grid = nextGrid;
+}
+
+function countNeighbourhood(col, row, selfStatus) {
+    var sum = 0;
+    for(var i = -1; i < 2; i++) {
+        for(var j = -1; j < 2; j++) {
+            if(grid[(col + i + cols) % cols][(row + j + rows) % rows]) sum++;
+        }
+    }
+    if(selfStatus) sum--;
+    return sum;
+}
+
+function nextGen() {
+    makeItLive();
+    render();
+    saveGrid();
+}
+
+function saveGrid() {
+    saveGrids.push(grid);
+    currentGen++;
+    document.querySelector('#genCounter').innerHTML = currentGen;
+}
+
+(function boot(){
+    populateField();
+    render();
+})()
+// var interval = setInterval(function() {nextGen()}, 200);
+
+document.querySelector('button').addEventListener('click', function() {
+    if(running) clearInterval(interval);
+    else interval = setInterval(function() {nextGen()}, 200);
+    running = !running;
+})
